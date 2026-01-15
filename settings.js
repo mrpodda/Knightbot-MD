8fdec41c5b62a5a74473f72dbdbbd6fcdac74519
mrpodda <kavzphotos0831@gmail.com>
2026-01-15 06:22:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Kavee Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Kavee Bot",
+  botOwner: 'Kaveesha', // Your name
+  ownerNumber: '94740170831', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is the bot developed by Methum Kaveesha.",
   version: "3.0.6",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
